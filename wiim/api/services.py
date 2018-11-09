b5bb846f1da9e4de927afabafae00c30d995e412
José Almeida <jose.afga@gmail.com>
2018-11-09 12:42:01 -0200

    added zone, site and server services

--- wiim/api/services.py
+++ wiim/api/services.py
@@ -9,7 +9,141 @@ Include Models management for API
 
 from flask import current_app as app
 # application imports
-from wiim.api.models import db, Process, Tag, Record, ProcessSchema, TagSchema, RecordSchema
+from wiim.api.models import *
+
+
+class SiteService():
+    """ Handle API for Sites """
+    @staticmethod
+    def create(name, comment=""):
+        """ Create a Site
+
+        keyword arguments:
+        name -- site name (required)
+        comment -- site comment or description (default "")
+        """
+        site = Site(
+            name=name,
+            comment=comment
+        )
+
+        # commit to database
+        db.session.add(site)
+        db.session.commit()
+
+        return {
+            'status': 'success',
+            'message': 'Site was created successfully!'
+        }  # created
+
+    @staticmethod
+    def get_all(page=1, count=0):
+        """ Get all Sites
+
+        keyword arguments:
+        page -- page number (default 1)
+        count -- tags per page, use zero for WIIM_COUNT_LIMIT (default 0)
+        """
+        sites_schema = SiteSchema(many=True)
+
+        # limit fetch quantity
+        if not count or count > app.config['WIIM_COUNT_LIMIT']:
+            count = app.config['WIIM_COUNT_LIMIT']
+
+        sites = Site.query.paginate(page, count).items
+        sites = sites_schema.dump(sites).data
+
+        return {'Sites': sites}
+
+    @staticmethod
+    def get_by_id(id):
+        """ Get single Site
+
+        keyword arguments:
+        id -- Site id (required)
+        """
+        site_schema = SiteSchema()
+
+        site = Site.query.get(id)
+        site = site_schema.dump(site).data
+
+        return site
+
+    @staticmethod
+    def update():
+        pass
+
+    @staticmethod
+    def destroy():
+        pass
+
+
+class ZoneService():
+    """ Handle API for Zones """
+    @staticmethod
+    def create(name, site_id, comment=""):
+        """ Create a Zone
+
+        keyword arguments:
+        name -- zone name (required)
+        site_id -- zone zone id (required)
+        comment -- zone comment or description (default "")
+        """
+        zone = Zone(
+            name=name,
+            site_id=site_id,
+            comment=comment
+        )
+
+        # commit to database
+        db.session.add(zone)
+        db.session.commit()
+
+        return {
+            'status': 'success',
+            'message': 'Zone was created successfully!'
+        }  # created
+
+    @staticmethod
+    def get_all(page=1, count=0):
+        """ Get all Zones
+
+        keyword arguments:
+        page -- page number (default 1)
+        count -- tags per page, use zero for WIIM_COUNT_LIMIT (default 0)
+        """
+        zones_schema = ZoneSchema(many=True)
+
+        # limit fetch quantity
+        if not count or count > app.config['WIIM_COUNT_LIMIT']:
+            count = app.config['WIIM_COUNT_LIMIT']
+
+        zones = Zone.query.paginate(page, count).items
+        zones = zones_schema.dump(zones).data
+
+        return {'Zones': zones}
+
+    @staticmethod
+    def get_by_id(id):
+        """ Get single Zone
+
+        keyword arguments:
+        id -- Zone id (required)
+        """
+        zone_schema = ZoneSchema()
+
+        zone = Zone.query.get(id)
+        zone = zone_schema.dump(zone).data
+
+        return zone
+
+    @staticmethod
+    def update():
+        pass
+
+    @staticmethod
+    def destroy():
+        pass
 
 
 class ProcessService():
@@ -58,7 +192,7 @@ class ProcessService():
         return {'Processes': processes}
 
     @staticmethod
-    def get(id):
+    def get_by_id(id):
         """ Get single Process
 
         keyword arguments:
@@ -70,6 +204,68 @@ class ProcessService():
         process = process_schema.dump(process).data
 
         return process
+
+    @staticmethod
+    def update():
+        pass
+
+    @staticmethod
+    def destroy():
+        pass
+
+
+class ServerService():
+    """ Handle API for Servers """
+    @staticmethod
+    def create(uid):
+        """ Create a Server
+
+        keyword arguments:
+        uid -- server uid (required)
+        """
+        server = Server(uid=uid)
+
+        # commit to database
+        db.session.add(server)
+        db.session.commit()
+
+        return {
+            'status': 'success',
+            'message': 'Server was created successfully!'
+        }  # created
+
+    @staticmethod
+    def get_all(page=1, count=0):
+        """ Get all Servers
+
+        keyword arguments:
+        page -- page number (default 1)
+        count -- tags per page, use zero for WIIM_COUNT_LIMIT (default 0)
+        """
+        servers_schema = ServerSchema(many=True)
+
+        # limit fetch quantity
+        if not count or count > app.config['WIIM_COUNT_LIMIT']:
+            count = app.config['WIIM_COUNT_LIMIT']
+
+        servers = Server.query.paginate(page, count).items
+        servers = servers_schema.dump(servers).data
+
+        return {'Servers': servers}
+
+    @staticmethod
+    def get_by_id(id):
+        """ Get single Server
+
+        keyword arguments:
+        id -- Server id (required)
+        """
+        server_schema = ServerSchema()
+
+        server = Process.query.get(id)
+        server = server_schema.dump(server).data
+
+        return server
 
     @staticmethod
     def update():
@@ -132,7 +328,7 @@ class TagService():
         return {'tags': tags}
 
     @staticmethod
-    def get(id):
+    def get_by_id(id):
         """ Get single Tag
 
         keyword arguments:
@@ -202,7 +398,7 @@ class RecordService():
         return {'records': records}
 
     @staticmethod
-    def get(id):
+    def get_by_id(id):
         """ Get single Record
 
         keyword arguments:
